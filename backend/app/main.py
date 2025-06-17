c57e7b473476834195379f401da06b7f1cd872c0
EnejiStats <enejistats@gmail.com>
2025-06-17 03:36:18 +0100

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/about", response_class=HTMLResponse)
